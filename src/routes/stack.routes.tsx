530c6378bdcf870e5a7ddca4568d5dcc1be404ef
Marcelo <mrclgst10@gmail.com>
2021-04-21 17:31:14 -0300

    feat: create stack routes

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Welcome } from '../pages/Welcome';
+import { UserIdentification } from '../pages/UserIdentification';
+import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
+
+import colors from '../styles/colors';
+
+const StackRoutes = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <StackRoutes.Navigator
+    headerMode="none"
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: colors.white
+      }
+    }}
+  >
+    <StackRoutes.Screen
+      name="Welcome"
+      component={Welcome}
+    />
+    <StackRoutes.Screen
+      name="UserIdentification"
+      component={UserIdentification}
+    />
+    <StackRoutes.Screen
+      name="Confirmation"
+      component={Confirmation}
+    />
+    <StackRoutes.Screen
+      name="PlantSelect"
+      component={PlantSelect}
+    />
+  </StackRoutes.Navigator>
+);
+
+export default AppRoutes;
