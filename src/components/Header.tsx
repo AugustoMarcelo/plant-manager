568dd414439d46b7b74ca5cb81673aeaeb31a9ae
Marcelo <mrclgst10@gmail.com>
2021-04-21 17:29:14 -0300

    feat: create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.username}>Marcelo</Text>
+      </View>
+      <Image source={{ uri: 'https://github.com/augustomarcelo.png' }} style={styles.image} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  username: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+  },
+})
