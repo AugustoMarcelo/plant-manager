86ccb2376ddc10dea532dc5468482b1d77d97a70
Marcelo <mrclgst10@gmail.com>
2021-04-21 17:31:28 -0300

    feat: create Load component

---
+++ src/components/Load.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native'
+import LottieView from 'lottie-react-native';
+
+import plantAnimation from '../assets/load.json';
+
+export function Load() {
+  return (
+    <View style={styles.container}>
+      <LottieView
+        source={plantAnimation}
+        autoPlay
+        loop
+        style={styles.animation}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  animation: {
+    backgroundColor: 'transparent',
+    height: 200,
+    width: 200,
+  }
+})
