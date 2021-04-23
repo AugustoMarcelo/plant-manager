1a998217dc9c5007d3485a69a2d9f1962e4f12b7
Marcelo <mrclgst10@gmail.com>
2021-04-22 23:08:13 -0300

    chore: create file types

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface PlantProps {
+  id: string;
+  name: string;
+  about: string;
+  water_tips: string;
+  photo: string;
+  environments: string[];
+  frequency: {
+    times: number;
+    repeat_every: string;
+  };
+  dateTimeNotification: Date;
+}
