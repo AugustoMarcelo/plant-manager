c2e758113db6e4bcd07b21d01a96a6c954592019
Marcelo <mrclgst10@gmail.com>
2021-04-22 23:08:58 -0300

    feat: add tab routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,9 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-import { PlantSelect } from '../pages/PlantSelect';
+import { PlantSave } from '../pages/PlantSave';
 
 import colors from '../styles/colors';
+import AuthRoutes from './tab.routes';
 
 const StackRoutes = createStackNavigator();
 
@@ -33,7 +34,15 @@ const AppRoutes: React.FC = () => (
     />
     <StackRoutes.Screen
       name="PlantSelect"
-      component={PlantSelect}
+      component={AuthRoutes}
+    />
+    <StackRoutes.Screen
+      name="PlantSave"
+      component={PlantSave}
+    />
+    <StackRoutes.Screen
+      name="MyPlants"
+      component={AuthRoutes}
     />
   </StackRoutes.Navigator>
 );
