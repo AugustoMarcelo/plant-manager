2b087f719d3f86ceba180a31dd90d800d2320e92
Marcelo <mrclgst10@gmail.com>
2021-04-22 23:09:32 -0300

    feat: get username from storage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Header() {
+  const [username, setUsername] = useState<string>();
+
+  useEffect(() => {
+    async function loadUsernameFromStorage() {
+      const name = await AsyncStorage.getItem('@plantmanager:user');
+      setUsername(name || '');
+    }
+
+    loadUsernameFromStorage();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View>
         <Text style={styles.greeting}>Olá,</Text>
-        <Text style={styles.username}>Marcelo</Text>
+        <Text style={styles.username}>{username}</Text>
       </View>
       <Image source={{ uri: 'https://github.com/augustomarcelo.png' }} style={styles.image} />
     </View>
