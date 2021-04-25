f83f1c97790b55e9901072d67632d47535be17c7
Marcelo <mrclgst10@gmail.com>
2021-04-25 11:35:49 -0300

    chore: configure Notifications

--- App.tsx
+++ App.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
-  useFonts, 
+  useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
-} from '@expo-google-fonts/jost'
-import AppLoading from 'expo-app-loading'
+} from '@expo-google-fonts/jost';
+import * as Notifications from 'expo-notifications';
+import AppLoading from 'expo-app-loading';
 import Routes from './src/routes';
+import { PlantProps } from './src/types';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
-    Jost_600SemiBold
+    Jost_600SemiBold,
   });
 
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+      }),
+    });
+
+    const subscription = Notifications.addNotificationReceivedListener(
+      async (notification) => {
+        const data = notification.request.content.data.plant as PlantProps;
+      }
+    );
+
+    return () => subscription.remove();
+    // async function notifications() {
+    //   const data = await Notifications.getAllScheduledNotificationsAsync();
+    //   console.log(data);
+
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+    // }
+
+    // notifications();
+  }, []);
+
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
-    <Routes />
-  );
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+      <Routes />
+    </>
+  )
 }
