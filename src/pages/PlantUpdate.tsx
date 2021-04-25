4efb910343ddd18b4026c3454223a58a4bbd1f03
Marcelo <mrclgst10@gmail.com>
2021-04-25 11:36:59 -0300

    feat: create page to update saved plants

---
+++ src/pages/PlantUpdate.tsx
@@ -0,0 +1,231 @@
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Platform,
+  Alert,
+  StatusBar,
+} from 'react-native';
+import { SvgFromUri } from 'react-native-svg';
+import { useNavigation, useRoute } from '@react-navigation/core';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
+import { format, isBefore } from 'date-fns';
+
+import { Button } from '../components/Button';
+import waterdropImg from '../assets/waterdrop.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { PlantProps } from '../types';
+import { savePlant } from '../libs/storage';
+import { EvilIcons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+
+interface Params {
+  plant: PlantProps;
+}
+
+export function PlantUpdate() {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { plant } = route.params as Params;
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date(plant.dateTimeNotification));
+  const [showDateTimePicker, setShowDateTimePicker] = useState(
+    Platform.OS === 'ios'
+  );
+
+  function handleChangeTime(event: Event, dateTime: Date | undefined) {
+    if (Platform.OS === 'android') {
+      setShowDateTimePicker((oldState) => !oldState);
+    }
+
+    if (dateTime && isBefore(dateTime, new Date())) {
+      setSelectedDateTime(new Date());
+      return Alert.alert('Opa', 'Escolha uma data válida ⏰');
+    }
+
+    if (dateTime) {
+      setSelectedDateTime(dateTime);
+    }
+  }
+
+  function handleOpenDateTimePickerForAndroid() {
+    setShowDateTimePicker((oldState) => !oldState);
+  }
+
+  async function handleUpdatePlant() {
+    try {
+      await savePlant({
+        ...plant,
+        dateTimeNotification: selectedDateTime,
+      });
+
+      navigation.navigate('Confirmation', {
+        title: 'Tudo certo',
+        subtitle:
+          'Fique tranquilo, pois sempre vamos lembra-lo de cuidar da sua plantinha com muito cuidado.',
+        buttonTitle: 'Muito obrigado',
+        icon: 'hug',
+        nextScreen: 'MyPlants',
+      });
+    } catch {
+      Alert.alert('Não foi possível salvar a planta');
+    }
+  }
+
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{ flex: 1, backgroundColor: colors.shape }}
+      contentContainerStyle={{ flex: 1 }}
+    >
+      <StatusBar barStyle="dark-content" backgroundColor={colors.shape} />
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <EvilIcons name="chevron-left" size={42} color={colors.heading} />
+        </TouchableOpacity>
+        <View style={styles.plantInfo}>
+          <SvgFromUri uri={plant.photo} height={210} width={210} />
+          <Text style={styles.plantName}>{plant.name}</Text>
+          <Text style={styles.plantAbout}>{plant.about}</Text>
+        </View>
+      </View>
+      <View style={styles.controller}>
+        <View style={styles.tipContainer}>
+          <Image source={waterdropImg} style={styles.tipImage} />
+          <Text style={styles.tipText}>{plant.water_tips}</Text>
+        </View>
+
+        <Text style={styles.alertLabel}>
+          Escolha o melhor horário para ser lembrado
+        </Text>
+
+        {showDateTimePicker && (
+          <DateTimePicker
+            value={selectedDateTime}
+            mode="time"
+            display="spinner"
+            onChange={handleChangeTime}
+          />
+        )}
+
+        {Platform.OS === 'android' && (
+          <TouchableOpacity onPress={handleOpenDateTimePickerForAndroid}>
+            <LinearGradient
+              style={styles.dateTimePickerButton}
+              colors={['#F5FAF7', '#F0F0F0']}
+              start={[0.1, 1]}
+              end={[0.5, 1]}
+            >
+              <Text style={styles.timeValue}>
+                {`${format(selectedDateTime, 'HH')} horas`}
+              </Text>
+              <Text style={styles.timeValue}>
+                {`${format(selectedDateTime, 'mm')} min`}
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        )}
+
+        <Button title="Confirmar alterações" onPress={handleUpdatePlant} />
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: colors.shape,
+    paddingBottom: 32,
+  },
+  backButton: {
+    position: 'absolute',
+    zIndex: 10,
+    top: 0,
+    left: 10,
+  },
+  plantInfo: {
+    flex: 1,
+    paddingHorizontal: 30,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: colors.shape,
+  },
+  plantName: {
+    fontFamily: fonts.heading,
+    fontSize: 24,
+    color: colors.heading,
+    marginTop: 15,
+  },
+  plantAbout: {
+    textAlign: 'center',
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+    marginTop: 10,
+  },
+  controller: {
+    flex: 1,
+    backgroundColor: colors.white,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: getBottomSpace() || 20,
+    justifyContent: 'space-between',
+  },
+  tipContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+    borderRadius: 20,
+  },
+  tipImage: {
+    width: 56,
+    height: 56,
+  },
+  tipText: {
+    flex: 1,
+    marginLeft: 20,
+    fontFamily: fonts.text,
+    color: colors.blue,
+    fontSize: 15,
+  },
+  alertLabel: {
+    textAlign: 'center',
+    fontFamily: fonts.complement,
+    color: colors.heading,
+    fontSize: 12,
+  },
+  dateTimePickerButton: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingVertical: 4,
+    paddingHorizontal: 20,
+    marginTop: 16,
+    marginBottom: 30,
+    backgroundColor: colors.shape,
+    borderRadius: 10,
+    width: '71%',
+  },
+  dateTimePickerText: {
+    color: colors.heading,
+    fontSize: 24,
+    fontFamily: fonts.text,
+  },
+  timeValue: {
+    fontFamily: fonts.text,
+    color: colors.heading,
+    fontSize: 17,
+  },
+});
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSave } from '../pages/PlantSave';
+import { PlantUpdate } from '../pages/PlantUpdate';
 
 import colors from '../styles/colors';
 import AuthRoutes from './tab.routes';
@@ -39,6 +40,10 @@ const AppRoutes: React.FC = () => (
     <StackRoutes.Screen
       name="PlantSave"
       component={PlantSave}
+    />
+    <StackRoutes.Screen
+      name="PlantUpdate"
+      component={PlantUpdate}
     />
     <StackRoutes.Screen
       name="MyPlants"
